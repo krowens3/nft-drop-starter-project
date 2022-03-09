84d1e375f4b195f523822f18bbf8d1833d29d707
Kevin Owens III <krowens3rd@gmail.com>
2022-03-09 07:59:35 -0800

    ✅ Wallet connection and check if already authoraized

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,54 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect } from 'react'
+import './App.css'
+import twitterLogo from './assets/twitter-logo.svg'
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = '_buildspace'
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  //Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window
+      if (solana && solana.isPhantom) {
+        console.log('Phanto wallet found')
+        const response = await solana.connect({ onlyIfTrusted: true })
+        console.log('Connected with public key:', response.publicKey.toString())
+      } else {
+        alert('Solana object not found! Please get a Phantom Wallet 👻')
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  //Useeffects
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected()
+    }
+    window.addEventListener('load', onLoad)
+    return () => window.removeEventListener('load', onLoad)
+  }, [])
   return (
-    <div className="App">
-      <div className="container">
-        <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
-          <p className="sub-text">NFT drop machine with fair mint</p>
+    <div className='App'>
+      <div className='container'>
+        <div className='header-container'>
+          <p className='header'>🍭 Candy Drop</p>
+          <p className='sub-text'>NFT drop machine with fair mint</p>
         </div>
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+        <div className='footer-container'>
+          <img alt='Twitter Logo' className='twitter-logo' src={twitterLogo} />
           <a
-            className="footer-text"
+            className='footer-text'
             href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
+            target='_blank'
+            rel='noreferrer'
           >{`built on @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
