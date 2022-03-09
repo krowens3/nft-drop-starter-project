3b24addb52c45b66d4866b1a8ae8855c8d79b328
Kevin Owens III <krowens3rd@gmail.com>
2022-03-09 08:04:32 -0800

    ✅ Connect wallet functionality

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import twitterLogo from './assets/twitter-logo.svg'
 
@@ -7,6 +7,7 @@ const TWITTER_HANDLE = '_buildspace'
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null)
   //Actions
   const checkIfWalletIsConnected = async () => {
     try {
@@ -15,6 +16,7 @@ const App = () => {
         console.log('Phanto wallet found')
         const response = await solana.connect({ onlyIfTrusted: true })
         console.log('Connected with public key:', response.publicKey.toString())
+        setWalletAddress(response.publicKey.toString())
       } else {
         alert('Solana object not found! Please get a Phantom Wallet 👻')
       }
@@ -22,6 +24,25 @@ const App = () => {
       console.error(error)
     }
   }
+
+  const connectWallet = async () => {
+    const { solana } = window
+
+    if (solana) {
+      const response = await solana.connect()
+      console.log('Connected with Public Key:', response.publicKey.toString())
+      setWalletAddress(response.publicKey.toString())
+    }
+  }
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className='cta-button connect-wallet-button'
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  )
   //Useeffects
   useEffect(() => {
     const onLoad = async () => {
@@ -36,6 +57,7 @@ const App = () => {
         <div className='header-container'>
           <p className='header'>🍭 Candy Drop</p>
           <p className='sub-text'>NFT drop machine with fair mint</p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className='footer-container'>
           <img alt='Twitter Logo' className='twitter-logo' src={twitterLogo} />
